536909f29929aa60f16068d5f27990dfc6fdfb35
pimben <lunelthomas974@gmail.com>
2026-02-19 16:04:06 +0100

    wikipetan

---
+++ scripts/content.js
@@ -0,0 +1,42 @@
+function renderReadingTime() {
+  
+
+  const image = document.querySelector("img[class='mw-logo-icon']");
+
+
+  image.src= randomWikipeTAn()
+  
+  image.style.height = '25%';
+  image.style.width = '25%';
+  
+  lol=document.getElementById("accueil_2017_bandeau")
+
+
+  lol.style.backgroundImage= "linear-gradient(rgba(255,255,255,0.4), rgba(255,255,255,0.4)), url('"+chrome.runtime.getURL('/images/wikipe-tan1.png')+"')"
+
+  lol.style.backgroundPosition= "0% -3%";
+  lol.style.backgroundSize= "250px 450px";
+  
+}
+
+function randomWikipeTAn() {
+  random = Math.floor(Math.random() * 6)+1
+
+  return chrome.runtime.getURL('/images/wikipe-tan'+random+'.png');
+}
+
+
+
+renderReadingTime();
+
+
+
+
+
+
+
+
+
+
+
+
